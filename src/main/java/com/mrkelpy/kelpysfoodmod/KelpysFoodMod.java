edcfeba5b60f9b5f79d451e394e3966b9bdf304d
Alexandre Silva <alexandresilva.coding@gmail.com>
2022-06-15 00:26:53 +0100

    First commit, added dango and mochi

---
+++ src/main/java/com/mrkelpy/kelpysfoodmod/KelpysFoodMod.java
@@ -0,0 +1,25 @@
+package com.mrkelpy.kelpysfoodmod;
+import com.mojang.logging.LogUtils;
+import com.mrkelpy.kelpysfoodmod.setup.Registration;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(KelpysFoodMod.MODID)
+public class KelpysFoodMod {
+    /**
+     * This class handles core interactions with Forge in order to make the mod work.
+     */
+
+    public static final String MODID = "kelpysfoodmod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public KelpysFoodMod() {
+        IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
+        Registration.ITEMS.register(modbus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+}
